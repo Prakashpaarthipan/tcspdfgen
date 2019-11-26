bce41667e62e27e2a20dde86efbd630082bb4447
Prakash <prakashkovaidt@gmail.com>
2019-11-26 16:44:13 +0530

    heroku test

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ var helmet = require('helmet');
 var ftp = require('basic-ftp');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -5,16 +5,40 @@ var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log(req.query.name);
     var sysdate = new Date();    
     var date = moment(sysdate).format('DD/MM/YYYY HH:mm');
-    var logfile = moment(sysdate).format('DD-MM-YYYY');
+    var file = moment(sysdate).format('DD-MM-YYYY');
     var month = moment(sysdate).format('MM');
     var ReplaceTime= date.replace(/[^0-9]/gi, '');
-    res.cookie('Times',ReplaceTime, {expire: 360000 + Date.now()}); 
-    var writeStream = fs.createWriteStream(logfile+".txt",{flags: 'a'});
+    
+    var logfile = file+""+req.query.name+".txt";
+    var writeStream = fs.createWriteStream("uploads/"+logfile,{flags: 'a'});
     writeStream.write(month+"::"+ReplaceTime+"\n");  
     writeStream.end();
-    res.send('respond with a resource : - '+ReplaceTime+' '+req.cookies.Times);
+    setTimeout(function(){
+       fs.copyFile("uploads/"+logfile, "public/txtfiles"+logfile, (err) => {
+      if (err) throw err;      
+    });
+    },2000);
+   
+    res.send('respond with a resource : - '+month+'::'+ReplaceTime+'\n');
   });
-  
+  router.get('/listall', function(req, res, next) { 
+    debugger;
+    console.log("TESTSEST");   
+        //passsing directoryPath and callback function
+        fs.readdir('uploads', function (err, files) {
+          //handling error
+          if (err) {
+              return console.log('Unable to scan directory: ' + err);
+          } 
+          //listing all files using forEach
+          files.forEach(function (file) {
+              // Do whatever you want to do with the file             
+              res.render('listfiles', { list: files });
+              next();
+          });
+        });           
+  });
   module.exports = router;
